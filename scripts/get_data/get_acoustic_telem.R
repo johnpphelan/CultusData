55b1f02c7b542afc57210b1d5fb6fc10d288340a
Phelan <John.Phelan@gov.bc.ca>
2024-09-24 16:27:00 -0700

    change to TRUE

--- scripts/get_data/get_acoustic_telem.R
+++ scripts/get_data/get_acoustic_telem.R
@@ -3,10 +3,8 @@ library(data.table)
 
 lan_folder = "//SFP.IDIR.BCGOV/S140/S40203/RSD_ FISH & AQUATIC HABITAT BRANCH/General/2 SCIENCE - Invasives/SPECIES/Smallmouth Bass/Cultus lake/"
 
-list.files(lan_folder)
 tag_names<-list.files(path = paste0(lan_folder,"2023 projects/acoustic telemetry/2023-10 downloads (Acoustic tag receiver data)"),
                     pattern = ".csv", full.names = T)
-tag_data23<-rbindlist(lapply(tag_names, fread), fill = T)
+tag_data23<-rbindlist(lapply(tag_names, fread), fill = TRUE)
 
-head(tag_data23)
 
