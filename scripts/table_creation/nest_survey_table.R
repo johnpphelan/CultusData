2e30d75b97203b519e5efd6d56f1e64491497953
Phelan <John.Phelan@gov.bc.ca>
2024-09-27 17:09:58 -0700

    nest survey table

---
+++ scripts/table_creation/nest_survey_table.R
@@ -0,0 +1,40 @@
+library(sqldf)
+library(dplyr)
+library(tidyr)
+
+source("scripts/utils/col_types_f.R")
+source("scripts/utils/fix_col_names_f.R")
+
+source("scripts/get_data/get_nest_survey.R")
+names(raw_nest_survey)
+nestRaw<- raw_nest_survey
+names(nestRaw)<-names_fix(names(nestRaw))
+names(nestRaw)<-remove_special_chars(names(nestRaw))
+names(nestRaw)<-gsub("_", "",names(nestRaw))
+
+str(nestRaw)
+
+
+db_filepath = "output/CultusData.sqlite"
+
+con<-dbConnect(RSQLite::SQLite(), db_filepath,extended_types = TRUE)
+
+nestRaw <- nestRaw |> 
+  mutate(nestID = row_number())
+
+col_types<-get_col_types(nestRaw)
+
+
+sur_col_types_sql <- col_types |> 
+  dplyr::mutate(key_status = case_when(
+    col_name %in% c("nestID") ~ "KEY",
+    TRUE ~ ""
+  )) |> 
+  dplyr::reframe(a = paste0(col_name, " ", stringr::str_to_upper(sqlite_type), " ", key_status))
+
+sql = paste0("CREATE TABLE IF NOT EXISTS nestRaw (
+       ",paste0(sur_col_types_sql$a,collapse = ",\n"),
+             ",\nPRIMARY KEY (nestID))")
+dbExecute(con, sql)
+dbWriteTable(conn = con, "nestRaw", nestRaw, row.names = F, append = T)
+dbListTables(con)
