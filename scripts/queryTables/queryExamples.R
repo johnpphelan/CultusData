2d75e45cd1bfa7a3081372bc10b623fd10cd9e7d
Phelan <John.Phelan@gov.bc.ca>
2024-12-11 08:59:55 -0800

    na

--- scripts/queryTables/queryExamples.R
+++ scripts/queryTables/queryExamples.R
@@ -13,7 +13,7 @@ con<-dbConnect(RSQLite::SQLite(), db_filepath)
 print(dbGetInfo(con))
 print(dbListObjects(con))
 
-query <- "PRAGMA table_info(scaleRawTable);"
+query <- "PRAGMA table_info(tagData);"
 result <- dbSendQuery(con, query)
 column_names <- fetch(result, n = -1)
 column_names
@@ -26,7 +26,7 @@ dbClearResult(result)
 
 
 
-query <- "SELECT * FROM iceData"
+query <- "SELECT * FROM surveyQuestions"
 dbExecute(con = con, query)
 #querydelete<-"DROP TABLE surveyData"
 result <- dbSendQuery(conn = con, query)
