fe943729323df4f975b3d3fd2d1f09de3fe746dd
Phelan <John.Phelan@gov.bc.ca>
2024-12-11 09:01:11 -0800

    change date to text for db

--- scripts/utils/col_types_f.R
+++ scripts/utils/col_types_f.R
@@ -3,8 +3,8 @@ get_col_types <- function(df) {
   sur_col_types <- data.frame(col_name = names(df),
                               type = as.vector(sapply(df, typeof))) %>%
     mutate(type = case_when(
-      col_name == "date" ~ "DATE",
-      col_name == "time" ~ "DATETIME",
+      col_name == "date" ~ "TEXT",
+      col_name == "time" ~ "TEXT",
       TRUE ~ type
     )) %>%
     mutate(sqlite_type = case_when(
@@ -12,8 +12,8 @@ get_col_types <- function(df) {
       type == "character" ~ "TEXT",
       type == "integer" ~ "INTEGER",
       type == "logical" ~ "INTEGER",
-      type == "Date" ~ "DATE",
-      type == "POSIXct" ~ "DATETIME",
+      type == "Date" ~ "TEXT",
+      type == "POSIXct" ~ "TEXT",
       TRUE ~ paste0(type)
     ))
   
