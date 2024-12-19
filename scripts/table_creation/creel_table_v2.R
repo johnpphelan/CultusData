6ff82f0b420922483585565d1b57e764933f23aa
Phelan <John.Phelan@gov.bc.ca>
2024-12-19 13:57:35 -0800

    fix the surveyNumber to start at one every day

--- scripts/table_creation/creel_table_v2.R
+++ scripts/table_creation/creel_table_v2.R
@@ -1,5 +1,6 @@
 library(sqldf)
 library(dplyr)
+library(lubridate)
 source("scripts/get_data/get_creel.R")
 source("scripts/utils/col_types_f.R")
 source("scripts/utils/fix_col_names_f.R")
@@ -15,6 +16,28 @@ survDT<- main_page |>
   select(Survey_No, Date, Time, Surveyor, Shift) |> 
   rename(surveyNumber = Survey_No, date = Date, time = Time, surveyor = Surveyor, shift = Shift) |> 
   mutate(date = as.character(date), time = as.character(time), shift = as.character(shift)) 
+
+#overwrite the survey_No column as per Amalis - this makes sure that the survey_No starts at 1 every day
+survDT<- survDT |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+survDT <- survDT |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
+  mutate(time = as.character(time))
+  
+
 
 sur_col_types <- get_col_types(survDT)
 
@@ -33,7 +56,7 @@ sur_col_types_sql <- sur_col_types |>
 
 sql = paste0("CREATE TABLE IF NOT EXISTS surveyData (
        ",paste0(sur_col_types_sql$a,collapse = ",\n"),
-             ",\nPRIMARY KEY (\nsurveyNumber\n, \ntime\n))")
+             ",\nPRIMARY KEY (\nsurveyNumber\n, \ndate\n))")
 
 
 dbExecute(con, sql)
@@ -53,16 +76,39 @@ dbClearResult(result)
 
 #####################################################################################################
 
+
+
 anglerData<- demography_edit |> 
   select(Survey, Date_Time, Gender, Age_Class, License_Period, Residency,
          City_Prov_Country, Postal_Code_first_3, Notes) |> 
-  rename(surveyNumber = Survey, time= Date_Time, gender=Gender, AgeClass = Age_Class,
+  rename(surveyNumber = Survey, time = Date_Time, gender=Gender, AgeClass = Age_Class,
          licensePeriod = License_Period, residency = Residency, cityProvinceCountry = City_Prov_Country,
          postCode = Postal_Code_first_3, notes = Notes) |> 
   mutate(date = as.Date(time)) |> 
   mutate(date = as.character(date), time = as.character(time)) |> 
   mutate(anglerID = row_number())
 
+
+
+
+anglerData<- anglerData |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+anglerData <- anglerData |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
+  mutate(time = as.character(time))
 
 col_types<-get_col_types(anglerData)
 
@@ -78,7 +124,7 @@ sur_col_types_sql
 sql = paste0("CREATE TABLE IF NOT EXISTS anglerInfo (
        ",paste0(sur_col_types_sql$a,collapse = ",\n"),
              ",\nPRIMARY KEY (anglerID),
-              \nFOREIGN KEY (surveyNumber, time) REFERENCES surveyData (surveyNumber, time))")
+              \nFOREIGN KEY (surveyNumber, date) REFERENCES surveyData (surveyNumber, date))")
 # 
 # drop_table_sql <- paste0("DROP TABLE IF EXISTS anglerInfo;")
 # 
@@ -108,6 +154,25 @@ catchDF<-main_page |>
   rename_with(~ str_replace_all(.x, "_", ""), everything()) |> 
   mutate(date = as.character(date), time = as.character(time)) 
 
+catchDF<- catchDF |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+catchDF <- catchDF |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
+  mutate(time = as.character(time))
+
 
 sur_col_types <- get_col_types(catchDF)
 
@@ -116,7 +181,7 @@ sur_col_types
 
 sur_col_types_sql <- sur_col_types |> 
   dplyr::mutate(key_status = case_when(
-    col_name %in% c("surveyNumber, time")  ~ "KEY",
+    col_name %in% c("surveyNumber, date, time")  ~ "KEY",
     TRUE ~ ""
   )) |> 
   dplyr::reframe(a = paste0(col_name, " ", stringr::str_to_upper(sqlite_type), " ", key_status))
@@ -125,7 +190,7 @@ sur_col_types_sql <- sur_col_types |>
 
 sql = paste0("CREATE TABLE IF NOT EXISTS fishCatch (
        ",paste0(sur_col_types_sql$a,collapse = ",\n"),
-             ",\nPRIMARY KEY (surveyNumber, time),
+             ",\nPRIMARY KEY (surveyNumber, date),
              \nFOREIGN KEY (surveyNumber, time) REFERENCES surveyData (surveyNumber, time),
              \nFOREIGN KEY (surveyNumber, time) REFERENCES anglerInfo (surveyNumber, time)
              )")
@@ -146,7 +211,7 @@ rowtoadd$date<-"2024-01-01"
 
 DBI::dbListTables(con)
 ##sucess! the foreign key means there must be a corresponding date added to surveyData 
-## before the fishCatch data can be updated.
+## before the fishCatch date can be updated.
 #dbWriteTable(con = con, name = "fishCatch", value = rowtoadd, append = T, row.names = F)
 
 ######################################################################################################
@@ -159,6 +224,26 @@ fishingDetails<- main_page |>
          personHoursFished = Total_Person_Hr_Fished, vessel = Vessel, prefferedSpp = Preferred_Catch_Spp,
          site = Site) |>   
   mutate(date = as.character(date), time = as.character(time))
+
+fishingDetails<- fishingDetails |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+fishingDetails <- fishingDetails |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
+  mutate(time = as.character(time), numberAnglers = as.character(numberAnglers), numberRods = as.character(numberRods))
+
 
 col_types<-get_col_types(fishingDetails)
 
@@ -189,10 +274,30 @@ dbClearResult(result)
 #######################################################################################################
 
 weatherTable<-main_page |> 
-  select(Survey_No, Time, Site, Mean_Air_Temperature, Cloud_Cover, Wind, Precip) |> 
-  rename(surveyNumber = Survey_No, time = Time, site = Site, meanAirTemp= Mean_Air_Temperature,
+  select(Survey_No, Date,  Time, Site, Mean_Air_Temperature, Cloud_Cover, Wind, Precip) |> 
+  rename(surveyNumber = Survey_No, date = Date, time = Time, site = Site, meanAirTemp= Mean_Air_Temperature,
          cloudCover = Cloud_Cover, wind = Wind, precip = Precip) |> 
   mutate(time = as.character(time)) 
+
+weatherTable<- weatherTable |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+weatherTable <- weatherTable |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
+  mutate(time = as.character(time))
+
 
 col_types<-get_col_types(weatherTable)
 
@@ -207,7 +312,7 @@ sur_col_types_sql <- col_types |>
 
 sql = paste0("CREATE TABLE IF NOT EXISTS weatherDetails (
        ",paste0(sur_col_types_sql$a,collapse = ",\n"),
-             ",\nPRIMARY KEY (surveyNumber, time, site),
+             ",\nPRIMARY KEY (surveyNumber, date, site),
               \nFOREIGN KEY (surveyNumber, time) REFERENCES surveyData (surveyNumber, time))")
 dbExecute(con, sql)
 dbWriteTable(conn = con, "weatherDetails", weatherTable, row.names = F, append = T)
@@ -225,14 +330,33 @@ questionTables <- questionTables |>
   rename(question = questionTables)
 
 answersTable<- main_page |> 
-  select(c(Survey_No, Time, contains(questionTables$question)))
+  select(c(Survey_No, Date, Time, contains(questionTables$question)))
 
 answersLong <- answersTable %>%
-  pivot_longer(cols = -c(Survey_No, Time), names_to = "Question", values_to = "Answer") |> 
+  pivot_longer(cols = -c(Survey_No, Time, Date), names_to = "Question", values_to = "Answer") |> 
   left_join(questionTables, by = c("Question" = "question")) |> 
   mutate(Question = questionID) |> 
   select(-questionID) |> 
-  rename( surveyNumber = Survey_No, time = Time, questionID = Question, answer = Answer) |> 
+  rename( surveyNumber = Survey_No, date = Date, time = Time, questionID = Question, answer = Answer) |> 
+  mutate(time = as.character(time))
+
+answersLong<- answersLong |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+answersLong <- answersLong |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
   mutate(time = as.character(time))
 
 
@@ -255,6 +379,9 @@ sql = paste0("CREATE TABLE IF NOT EXISTS surveyAnswers (
               \nFOREIGN KEY (surveyNumber, time) REFERENCES anglerInfo (surveyNumber, time),
               \nFOREIGN KEY (surveyNumber, time) REFERENCES surveyData (surveyNumber, time)
              )")
+
+answersLong <- answersLong |> 
+  mutate(date = as.character(date))
 
 dbExecute(con, sql)
 dbWriteTable(conn = con, "surveyAnswers", answersLong, row.names = F, append = T)
@@ -297,6 +424,25 @@ ICE<-ICE |>
   mutate(date = as.character(date), time = as.character(time)) |> 
   mutate(iceID = row_number())
 
+ICE<- ICE |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+ICE <- ICE |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
+  mutate(time = as.character(time))
+
 sur_col_types <- get_col_types(ICE)
 
 sur_col_types
@@ -338,6 +484,26 @@ fishcaught<- fish_edit |>
   mutate(date = as.character(date), time = as.character(time),
          fishID = row_number())
 
+fishcaught<- fishcaught |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+fishcaught <- fishcaught |> 
+  mutate(time = ymd_hms(time)) |> 
+  group_by(time) |> 
+  mutate(
+    time = if_else(row_number() > 1, time + minutes(5) * (row_number() - 1), time)
+  ) |> 
+  ungroup() |> 
+  mutate(time = as.character(time))
+
+
 sur_col_types <- get_col_types(fishcaught)
 
 sur_col_types
@@ -345,7 +511,7 @@ sur_col_types
 
 sur_col_types_sql <- sur_col_types |> 
   dplyr::mutate(key_status = case_when(
-    col_name %in% c("surveyNo, time, fishID")  ~ "KEY",
+    col_name %in% c("surveyNumber, time, fishID")  ~ "KEY",
     TRUE ~ ""
   )) |> 
   dplyr::reframe(a = paste0(col_name, " ", stringr::str_to_upper(sqlite_type), " ", key_status))
@@ -372,3 +538,4 @@ dbClearResult(result)
 
 
 dbDisconnect(con)
+
