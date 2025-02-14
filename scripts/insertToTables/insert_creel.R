f3b762ae3166a0b25117fd43e805fa1e14939b35
Phelan <John.Phelan@gov.bc.ca>
2025-02-13 17:03:51 -0800

    change creel

--- scripts/insertToTables/insert_creel.R
+++ scripts/insertToTables/insert_creel.R
@@ -43,7 +43,7 @@ names(creelData)<-namesFix
 
 sheets<-excel_sheets(paste0(lan_folder,"Cultus Lake Creel Form 2024_Data_Working2.xlsx"))
 sheetOI<-grep("Demographic", sheets, value = TRUE)
-demograhpy<-lapply(sheetOI, read_excel, path = f_name)
+demography<-lapply(sheetOI, read_excel, path = f_name)
 
 sheetOI<-grep("Fish", sheets, value = TRUE)
 fishdata<-lapply(sheetOI, read_excel, path = f_name)
@@ -52,16 +52,21 @@ sheetOI<-grep("ICE", sheets, value = TRUE)
 ICE<-data.frame(lapply(sheetOI, read_excel, path = f_name))
 
 
-creelData<-creelData |> 
+
+#################################################################
+
+#########
+
+
+creelData<-creelData |>
   mutate(Time = as_datetime(as.numeric(Time) * 86400, origin = "1899-12-30")) |>  # Convert Excel time to datetime
-  group_by(Time) |> 
+  group_by(Time) |>
   mutate(
     Time = if_else(row_number() > 1, Time + minutes(5) * (row_number() - 1), Time) # Add 5 minutes to duplicates
-  ) |> 
-  ungroup() 
+  ) |>
+  ungroup()
   
-  
-  
+
 
 survDT<- creelData |> 
   select(Survey_No, Date, Time, Surveyor, Shift) |> 
@@ -94,10 +99,20 @@ survDT<- survDT |>
          shift_end_time = as.character(paste(date, shift_end_time))) |> 
   select(surveyNumber, date, time, surveyor, shift_start_time, shift_end_time, hours_worked)
 
+survDT <- survDT |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
 
 
-recentID<-dbGetQuery(conn = con, "SELECT surveyNumber FROM surveyData")
-dbAppendTable(con, "surveyData", survDT) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
+
+#recentID<-dbGetQuery(conn = con, "SELECT surveyNumber FROM surveyData")
+dbAppendTable(con, "creelShifts", survDT) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
 
 #############################################################################################################
 
@@ -122,30 +137,30 @@ catchDF<- catchDF |>
   mutate(date = as.character(date))
 
 
-dbAppendTable(con, "fishCatch", catchDF) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
+dbAppendTable(con, "creelFishResults", catchDF) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
 
 ##########################################################################
 
-fishingdetails<- creelData |> 
-  select(Survey_No, Date, Time, No_Anglers, No_Rods, Total_Person_Hr_Fished, Vessel,
-         Preferred_Catch_Spp, Site) |> 
-  mutate(Date = as.Date(Date, origin = "1899-12-30")) |> 
-  rename(surveyNumber = Survey_No, date = Date, time = Time, numberAnglers = No_Anglers, numberRods = No_Rods, 
-         personHoursFished = Total_Person_Hr_Fished, vessel = Vessel, prefferedSpp = Preferred_Catch_Spp,
-         site = Site) |>   
-  mutate(date = as.character(date), time = as.character(time))
-
-fishingdetails<- fishingdetails |> 
-  mutate(date = as.Date(date)) |> 
-  arrange(date) |> 
-  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
-  group_by(date_group) |> 
-  mutate(surveyNumber = row_number()) |>  
-  ungroup() |> 
-  select(-date_group) |> 
-  mutate(date = as.character(date))
-
-dbAppendTable(con, "fishingDetails", fishingdetails) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
+# fishingdetails<- creelData |> 
+#   select(Survey_No, Date, Time, No_Anglers, No_Rods, Total_Person_Hr_Fished, Vessel,
+#          Preferred_Catch_Spp, Site) |> 
+#   mutate(Date = as.Date(Date, origin = "1899-12-30")) |> 
+#   rename(surveyNumber = Survey_No, date = Date, time = Time, numberAnglers = No_Anglers, numberRods = No_Rods, 
+#          personHoursFished = Total_Person_Hr_Fished, vessel = Vessel, prefferedSpp = Preferred_Catch_Spp,
+#          site = Site) |>   
+#   mutate(date = as.character(date), time = as.character(time))
+# 
+# fishingdetails<- fishingdetails |> 
+#   mutate(date = as.Date(date)) |> 
+#   arrange(date) |> 
+#   mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+#   group_by(date_group) |> 
+#   mutate(surveyNumber = row_number()) |>  
+#   ungroup() |> 
+#   select(-date_group) |> 
+#   mutate(date = as.character(date))
+# 
+# dbAppendTable(con, "fishingDetails", fishingdetails) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
 
 #####################################################################
 
@@ -166,7 +181,7 @@ weatherTable<- weatherTable |>
   select(-date_group) |> 
   mutate(Date = as.character(Date))
 
-dbAppendTable(con, "weatherDetails", weatherTable) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
+dbAppendTable(con, "creelWeather", weatherTable) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
 
 
 ############################################################################
@@ -179,7 +194,7 @@ questionTable <- questionTable |>
   slice(32:43) |> 
   rename(question = questionTable)
 
-query <- "SELECT * FROM surveyQuestions"
+query <- "SELECT * FROM creelSurveyQuestions"
 dbExecute(con = con, query)
 #querydelete<-"DROP TABLE surveyData"
 result <- dbSendQuery(conn = con, query)
@@ -193,11 +208,11 @@ miss<- questionTable |>
 adding<- miss |> 
   mutate(questionID = max(df$questionID) + row_number())
 
-dbAppendTable(con, "surveyQuestions", adding) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
+dbAppendTable(con, "creelSurveyQuestions", adding) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
 
 #####################################################################################
 
-query <- "SELECT * FROM surveyQuestions"
+query <- "SELECT * FROM creelSurveyQuestions"
 dbExecute(con = con, query)
 #querydelete<-"DROP TABLE surveyData"
 result <- dbSendQuery(conn = con, query)
@@ -233,7 +248,7 @@ answersLong<- answersLong |>
   mutate(date = as.character(date))
 
 
-dbAppendTable(con, "surveyAnswers", answersLong) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
+dbAppendTable(con, "creelSurveyAnswers", answersLong) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
 
 
 #####################################################################################################
@@ -242,7 +257,7 @@ dbAppendTable(con, "surveyAnswers", answersLong) ## adds these - if the entry (s
 
 
 
-query <- "SELECT * FROM surveyAnswers"
+query <- "SELECT * FROM creelSurveyAnswers"
 dbExecute(con = con, query)
 #querydelete<-"DROP TABLE surveyData"
 result <- dbSendQuery(conn = con, query)
@@ -256,18 +271,116 @@ dbClearResult(result)
 #################################################################
 
 
-
-ICE2<- ICE |> 
-  rename(date = Date, time = Time, noAnglingBoats = X..angling.boats, noBoatAnglers = X..boat.anglers, 
-         noShoreAnglers = X..shore.anglers, noDockAnglers=X..dock.anglers, airTemp = Air.temperature,
-         cloudCover = Cloud.cover, wind = Wind, precip = Precipitation) |> 
-  mutate(weather = paste0("Air temperature: ", airTemp,", cloud cover: ", cloudCover, ", wind: ", wind,", precipiation: ", precip)) |> 
-  select(date, time, noAnglingBoats, noBoatAnglers, noShoreAnglers, noDockAnglers, weather)
-
-
-dbAppendTable(con, "iceData", ICE2) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
-
-
+# 
+# ICE2<- ICE |> 
+#   rename(date = Date, time = Time, noAnglingBoats = X..angling.boats, noBoatAnglers = X..boat.anglers, 
+#          noShoreAnglers = X..shore.anglers, noDockAnglers=X..dock.anglers, airTemp = Air.temperature,
+#          cloudCover = Cloud.cover, wind = Wind, precip = Precipitation) |> 
+#   mutate(weather = paste0("Air temperature: ", airTemp,", cloud cover: ", cloudCover, ", wind: ", wind,", precipiation: ", precip)) |> 
+#   select(date, time, noAnglingBoats, noBoatAnglers, noShoreAnglers, noDockAnglers, weather)
+# 
+# 
+# dbAppendTable(con, "iceData", ICE2) ## adds these - if the entry (surveyNumber and time) already exists, then it won't work
 
 
 
+fishResults<- creelData |> 
+  select(Survey_No, Date, Time, Total_Fish_Caught, Total_Retained, ends_with("_c"), ends_with("_r"),
+         Release_Reason, Total_Person_Hr_Fished, No_Anglers, No_Rods, Vessel, Preferred_Catch_Spp, Site) |> 
+  rename(surveyNumber = Survey_No, date = Date, time = Time, totFishCaught = Total_Fish_Caught,
+         totRetained = Total_Retained, releaseReason = Release_Reason, personHours = Total_Person_Hr_Fished,
+         noAnglers = No_Anglers, noRods = No_Rods, vessl = Vessel, preferredSpp = Preferred_Catch_Spp, site = Site ) |>
+  rename_with(~ str_replace_all(.x, "_", ""), everything()) |> 
+  mutate(date = as.character(date), time = as.character(time)) 
+
+fishResults <- fishResults |>
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+dbAppendTable(con, "creelFishResults", fishResults)
+
+##########################################################################
+
+demo<-demography[[1]]
+
+demo<- demo |> 
+  rename(surveyNumber = `Survey #`, dateTime = `Date & Time`, gender = Gender, ageClass = `Age Class`, 
+         licensePeriod = `License Period`, residency = Residency, cityProvinceCountry = `City, Prov, Country`, 
+         postCode = `Postal Code (first 3)`, notes = Notes)
+
+#anglerData<- 
+anglerData<- demo |> 
+  mutate(date = as.Date(dateTime), time = as_datetime(dateTime)) |> 
+  mutate(date = as.Date(time)) |> 
+  mutate(date = as.character(date), time = as.character(time)) |> 
+  mutate(anglerID = row_number()) |> 
+  select(-dateTime)
+
+
+
+
+anglerData<- anglerData |> 
+  mutate(date = as.Date(date)) |> 
+  arrange(date) |> 
+  mutate(date_group = cumsum(date != lag(date, default = first(date)))) |> 
+  group_by(date_group) |> 
+  mutate(surveyNumber = row_number()) |>  
+  ungroup() |> 
+  select(-date_group) |> 
+  mutate(date = as.character(date))
+
+query <- "SELECT * FROM creelFisherDemography"
+dbExecute(con = con, query)
+#querydelete<-"DROP TABLE surveyData"
+result <- dbSendQuery(conn = con, query)
+df<-fetch(result, -1)
+df
+dbClearResult(result)
+
+
+anglerData<- anglerData |> 
+  mutate(anglerID = max(df$anglerID) + row_number())
+
+
+dbAppendTable(con, "creelFisherDemography", anglerData)
+
+#################################################################################################
+
+
+################### Add the fish data to creelFishDetails
+query <- "SELECT * FROM creelFishDetails"
+dbExecute(con = con, query)
+#querydelete<-"DROP TABLE surveyData"
+result <- dbSendQuery(conn = con, query)
+df<-fetch(result, -1)
+df
+dbClearResult(result)
+
+fd<-fishdata[[1]]
+
+fishd <-fd |> 
+  rename(surveyNumber = `Survey #`, dateTime = `Date & Time`, fishNo = `Fish #`,
+         length = `Length, mm`, weight = `Weight, g`, pitTagNo = `Pit Tag #`, acousticTagNo = `Acoustic tag #`, 
+         notes = Notes)
+
+fishd <- fishd |>
+  mutate(date = as.Date(dateTime), time = as_datetime(dateTime)) |> 
+  mutate(date = as.Date(time)) |> 
+  mutate(date = as.character(date), time = as.character(time)) |> 
+  select(-dateTime)
+
+fishd<- fishd |> 
+  mutate(fishID = max(df$fishID) + row_number())
+
+#add the new columns
+dbExecute(con, "ALTER TABLE creelFishDetails ADD COLUMN acousticTagNo TEXT")
+
+dbAppendTable(con, "creelFishDetails", fishd)
+
+dbDisconnect(con)
