c7f73bdd556406f9830748c3d5db96baacde9262
Phelan <John.Phelan@gov.bc.ca>
2026-05-05 13:42:00 -0700

    working 2025 script. Need to fix start and endtime

--- scripts/insertToTables/insert_nest_data_2025.R
+++ scripts/insertToTables/insert_nest_data_2025.R
@@ -5,6 +5,7 @@ library(stringr)
 library(openxlsx)
 library(DBI)
 library(tidyr)
+library(sf)
 source("scripts/utils/fix_col_names_f.R")
 source("scripts/insertToTables/insert_creel_functions.R")
 
@@ -52,129 +53,84 @@ leaflet() %>%
 
 library(ggplot2)
 library(sf)
-# ggplot() +
-#   geom_sf(data = bc, color = "grey") +
-#   geom_sf(data = nestData, aes(color = 1:nrow(latfix))) +
-#   scale_color_viridis_c(option = "plasma") +
-#   geom_sf_text(data = latfix, aes(label = 1:nrow(latfix)), size = 3, nudge_y = 0.001)
-# 
-# latfix[25,]$northing
-# latfix$northing[latfix$northing == 1299517] <- 1293517
-# latfix[25,]$northing <- 1293517
-# latfix[25,]$easting <- 444402.7
-# 
-# latfix[25,]$northing
-# latfix[25,]$easting
-# latfix <- latfix |> 
-#   st_drop_geometry() |> 
-#   st_as_sf(coords = c("northing","easting"), crs = 3005) |> 
-#   st_transform(32610)
-# 
-# ggplot() +
-#   geom_sf(data = bc, color = "grey") +
-#   geom_sf(data = latfix, aes(color = 1:nrow(latfix))) +
-#   scale_color_viridis_c(option = "plasma") +
-#   geom_sf_text(data = latfix, aes(label = 1:nrow(latfix)), size = 3, nudge_y = 0.001)
-
-
-# fixes the instance where the position was given a different CRS
-# east<-nestData |> 
-#   filter(!str_detect(Latitude, "[0-9]{2}\\.")) |> 
-#   mutate(new_north = ifelse(str_count(Latitude, "[0-9]")==7, Latitude, Longitude),
-#          new_east = ifelse(str_count(Longitude, "[0-9]")==6, Longitude, Latitude)
-#          ) |> 
-#   sf::st_as_sf(coords = c( "new_east","new_north"), crs = 32610) |> 
-#   mutate(easting = sf::st_coordinates(geometry)[,2],
-#          northing = sf::st_coordinates(geometry)[,1],
-#   ) |> 
-#   select(-c(Latitude, Longitude))
-# 
-# ggplot() +
-#   geom_sf(data = bc, color = "grey") +
-#   geom_sf(data = east, aes(color = 1:nrow(east))) +
-#   scale_color_viridis_c(option = "plasma") +
-#   geom_sf_text(data = east, aes(label = 1:nrow(east)), size = 3, nudge_y = 0.001)
-# 
-# latfix <- latfix |> 
-#   mutate(easting = sf::st_coordinates(geometry)[,2],
-#          northing = sf::st_coordinates(geometry)[,1],
-#   ) |> 
-#   st_drop_geometry()
-# 
-# east <- east |> 
-#   st_drop_geometry()
-# 
-# 
-# blanks<- nestData |> 
-#   filter(is.na(Latitude)) |> 
-#   rename(easting = Latitude, northing = Longitude)
-# 
-#   
-# nestdataClean<-rbind(latfix, east)
-# nestdataClean<-rbind(nestdataClean, blanks)
-
-
-
-# nestSF<- sf::st_as_sf(nestdataClean[!is.na(nestdataClean$easting),], coords = c("northing", "easting"), crs = 32610)
-# 
-# ggplot() +
-#   geom_sf(data = bc, color = "grey") +
-#   geom_sf(data = nestSF, aes(color = 1:nrow(nestSF))) +
-#   scale_color_viridis_c(option = "plasma") +
-#   geom_sf_text(data = nestSF, aes(label = 1:nrow(nestSF)), size = 3, nudge_y = 0.001)
-
-
-
-
-
-
-# test <- nestData |> 
-#   mutate(Nests.Destroyed = case_when(
-#     !is.na(Nests.Destroyed) & Nests.Destroyed == "Y" ~ "Nest destroyed.",
-#     !is.na(Nests.Destroyed) & Nests.Destroyed == "N" ~ "Nest not destroyed.",
-#     !is.na(Nests.Destroyed) & Nests.Destroyed == "P" ~ "Nest partially destroyed.",
-#     !is.na(Nests.Destroyed) & Nests.Destroyed == "n/a" ~ "",
-#     !is.na(Nests.Destroyed) & Nests.Destroyed == "N (prev Y)" ~ "Nest not destroyed. ",
-#     is.na(Nests.Destroyed) ~ "", # or any other desired value for NA
-#     TRUE ~ ""
-#   )) |> 
-#   mutate(Fry.Captured = case_when(
-#     !is.na(Fry.Captured) & Fry.Captured == "N" ~ "No fry captured. ",
-#     !is.na(Fry.Captured) & Fry.Captured == "Y" ~ "Fry captured. ",
-#     !is.na(Fry.Captured) & Fry.Captured == "Y (3)" ~ "Three fry captured. ",
-#     !is.na(Fry.Captured) & Fry.Captured == "Y (2)" ~ "Two fry captured. ",
-#     !is.na(Fry.Captured) & Fry.Captured == "Y (1)" ~ "Fry captured. ",
-#     !is.na(Fry.Captured) & Fry.Captured == "N (attempted)" ~ "Tried to catch fry, unsuccessful. ",
-#     !is.na(Fry.Captured) & Fry.Captured == "n/a" ~ "",
-#     is.na(Fry.Captured) ~ "",
-#     TRUE ~ ""
-#   )) |> 
-#   mutate(activity = paste(Nests.Destroyed, Fry.Captured, sep = ". ")) |> 
-#   mutate(comments = paste(comments, location, ". Substrate:",Substrate, sep = " "))
-#   
+ 
 
 nestDF<- nestData |> 
-  rename(date = Date, easting = Easting, northing = Northing, 
-         depth = Depth..m., diameter = Diameter..m., guarding = Gaurding.Male..Y.N.,
+  rename(date = Date, 
+         depth = Depth..m., diameter = Diameter..m., guarding = Guarding.Male..Y.N.,
          lifeStage = Life.stage.of.nest,
-         adjacentStructure = Adjacent.structure,
+         adjacentStrucuture = Adjacent.structure,
          habitatType = Habitat.type, 
-         
-         
-         activityCompleted = activity.completed.by.surveyor, 
+         startTime = Start.Time,
+         endTime = End.Time,
+         location = Location.Name,
+         surfaceTemp = Surface..Temp,
+         locationMarker = Location.Marker,
+         otherInvasives = Other.invasive.species.present.,
+         activityCompleted = Activity.completed.by.surveyor, 
          nestDestroyed = Nest.fully.destroyed.,
-         
-         comments = comments) |> 
-  select(-c(locs, Fry.Captured, location))
+         substrate = Habitat.type,
+         comments = Comments)
+
+## merge locationMarker and adjacentStrucuture, remove them both and then rename it adjacentStrucutre
+
+
+nestDF <- nestDF |>
+  dplyr::mutate(
+    adjacentStructure = dplyr::if_else(
+      !is.na(adjacentStrucuture) & !is.na(locationMarker),
+      paste(adjacentStrucuture, locationMarker, sep = "; "),
+      dplyr::coalesce(adjacentStrucuture, locationMarker)
+    )
+  ) |> 
+  dplyr::select(-locationMarker, -adjacentStrucuture) |> 
+  sf::st_drop_geometry()
+
+
 
 nestDF <- nestDF |> 
   mutate(date = as.character(date))
 
+nestDF <- nestDF |>
+  mutate(
+    startTime = as.POSIXct(startTime * 86400,
+                         origin = "1899-12-30",
+                         tz = "UTC"),
+    endTime = as.POSIXct(endTime * 86400,
+                         origin = "1899-12-30",
+                         tz = "UTC")
+  )
+
+
+nestDF |>
+  group_by(date) |>
+  summarise(count = n(), .groups = "drop")
+
 ### Adding new columns to the data table 
-addCol<-"ALTER TABLE nestRaw ADD COLUMN AIS VARCHAR"
+# addCol<-"ALTER TABLE nestRaw ADD COLUMN AIS VARCHAR"
+# dbExecute(con = con, addCol)
+# addCol<-"ALTER TABLE nestRaw ADD COLUMN Substrate VARCHAR"
+# dbExecute(con = con, addCol)
+# delCol<-"ALTER TABLE nestRaw DROP COLUMN locationMarker"
+# dbExecute(con = con, delCol)
+addCol<-"ALTER TABLE nestRaw ADD COLUMN startTime VARCHAR"
 dbExecute(con = con, addCol)
-addCol<-"ALTER TABLE nestRaw ADD COLUMN Substrate VARCHAR"
+
+addCol<-"ALTER TABLE nestRaw ADD COLUMN endTime VARCHAR"
 dbExecute(con = con, addCol)
+
+addCol<-"ALTER TABLE nestRaw ADD COLUMN Participants VARCHAR"
+dbExecute(con = con, addCol)
+
+addCol<-"ALTER TABLE nestRaw ADD COLUMN location VARCHAR"
+dbExecute(con = con, addCol)
+
+addCol<-"ALTER TABLE nestRaw ADD COLUMN surfaceTemp VARCHAR"
+dbExecute(con = con, addCol)
+
+addCol<-"ALTER TABLE nestRaw ADD COLUMN otherInvasives VARCHAR"
+dbExecute(con = con, addCol)
+
 
 
 dbAppendTable(con, "nestRaw", nestDF)
